dd72ab1261e861105ae13710caaa436986b56c09
Mark Kegel <mark.kegel@gmail.com>
2021-11-23 09:32:04 -0600

    wip

--- compiler/test/dotty/tools/dotc/inlinetrait/InlineTraitCompiling.scala
+++ compiler/test/dotty/tools/dotc/inlinetrait/InlineTraitCompiling.scala
@@ -10,6 +10,27 @@ import dotty.tools.dotc.core.Contexts.{Context, ctx}
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
+//object example {
+//  import scala.quoted._
+//
+//  def somemacroimpl(b: Expr[Boolean])(using Quotes): Expr[String] = {
+//    if (b.valueOrError)
+//      '{"true"}
+//    else
+//      '{"false"}
+//  }
+//
+//  inline def somemacro(inline b: Boolean): String =
+//    ${somemacroimpl('b)}
+//}
+//
+//object uses {
+//  import example._
+//
+//  somemacro(true)
+//}
+
+
 class InlineTraitCompiling extends DottyTest {
 
   private def newContext = {
@@ -25,7 +46,22 @@ class InlineTraitCompiling extends DottyTest {
 
       import scala.quoted._
 
-      def swizzle(b: Expr[Boolean])(using Quotes): Expr[Any] = {
+      object some_macros {
+        def somemacroimpl(b: Expr[Boolean])(using Quotes): Expr[String] = {
+          if (b.valueOrError)
+            '{"true"}
+          else
+            '{"false"}
+        }
+
+        inline def somemacro(inline b: Boolean): String = $${somemacroimpl('b)}
+
+        somemacro(true)
+
+      }
+
+      // Should return a TraitBody, but Any will work for today
+      def traitBody(b: Expr[Boolean])(using Quotes): Expr[Any] = {
         if (b.valueOrError)
           // inject a method named foo: Int = 1
           // have to quote to capture the AST
@@ -35,44 +71,15 @@ class InlineTraitCompiling extends DottyTest {
           '{def bar: String = "1"}
       }
 
-      inline trait Too(inline b: Boolean) { self =>
+      inline trait IfTrueThenFooElseBar(inline b: Boolean) { self =>
         // with an inline defs wants this to be a call to a static method returning an Expr[T]
-        $${swizzle('b)}
-      }
-
-      inline trait Fizz(inline b: Boolean) {
-        // statements are evaluated like expressions in an inline trait
-        inline if (b) {
-          def[this] foo: Int = 1
-        } else {
-          def[this] bar: String = "1"
-        }
+        $${traitBody('b)}
       }
 
       // want syntax to evaluate a statement in a particular implicit context (override context)
 
-      object Fizz extends Too(true)
+      object ExpectMethodFoo extends IfTrueThenFooElseBar(true)
     """
-
-    inline trait Fizz(inline b: Boolean) { self =>
-      // want a shortcut for this kind of syntax
-      inline if (b) {
-        //summon[StatementContext]
-        // treat as an erased value
-        //given StatementContext = this
-        // shortcut could be what?
-
-        // other keywords to leverage?
-        //export
-        //extension
-
-        // how to lift a statement and turn it into data?
-
-        def[this] foo: Int = 1
-      } else {
-        def[this] bar: String = "1"
-      }
-    }
 
     checkCompile("typer", source) { (tree, context) =>
       given Context = context
@@ -87,3 +94,23 @@ class InlineTraitCompiling extends DottyTest {
     }
   }
 }
+
+    // inline trait Fizz(inline b: Boolean) { self =>
+    //   // want a shortcut for this kind of syntax
+    //   inline if (b) {
+    //     //erased val sc = summon[StatementContext]
+    //     // treat as an erased value
+    //     //given StatementContext = this
+    //     // shortcut could be what?
+
+    //     // other keywords to leverage?
+    //     //export
+    //     //extension
+
+    //     // how to lift a statement and turn it into data?
+
+    //     def[this] foo: Int = 1
+    //   } else {
+    //     def[this] bar: String = "1"
+    //   }
+    // }
